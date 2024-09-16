eccce04f248728e856f18646fdc5787a755ae011
zw <zhanghuohuo121@163.com>
2024-09-16 17:48:46 +0800

    Delete upload/upload/2024/06/02 directory

--- pmhub-base/pmhub-base-security/src/main/java/com/laigeoffer/pmhub/base/security/annotation/EnableDistributedLock.java
+++ pmhub-base/pmhub-base-security/src/main/java/com/laigeoffer/pmhub/base/security/annotation/EnableDistributedLock.java
@@ -10,9 +10,14 @@ import java.lang.annotation.*;
  * @description EnableDistributedLock 元注解，开启分布式锁功能
  * @create 2024-06-17-10:56
  */
-@Target(ElementType.TYPE)
-@Retention(RetentionPolicy.RUNTIME)
-@Documented
+@Target(ElementType.TYPE)               // 指定了 EnableDistributedLock 注解可以应用于类、接口声明
+@Retention(RetentionPolicy.RUNTIME)     // 指定了 EnableDistributedLock 注解在运行时是可用的
+@Documented                             // 表明 EnableDistributedLock 注解会被 javadoc 工具记录，注解的文档将被包含在 javadoc 中
+
+// 导入 Spring 框架的组件。
+// 当使用 @EnableDistributedLock 注解时
+// Spring 容器会自动注册 DistributedLockAspect 作为一个 Bean
+// 从而激活分布式锁的 AOP 功能。
 @Import({DistributedLockAspect.class})
 public @interface EnableDistributedLock {
 }
